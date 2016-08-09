308b1710855d7c985a7d721bad09a31f186bd228
Emil Vikström <emil@pixelstore.se>
2016-08-09 09:05:57 +0200

    Move loose functions into a CandyCrush class

--- candycrush.py
+++ candycrush.py
@@ -8,28 +8,34 @@ def scaler(OldMin, OldMax, NewMin, NewMax):
         return (((OldValue - OldMin) * (NewMax - NewMin)) / (OldMax - OldMin)) + NewMin
     return fn
 
-def setup_servod():
-    if not os.path.exists("/dev/servoblaster"):
-        subprocess.call(["servod", "--idle-timeout=1000ms"])
-    
 
-def set_servo(physical_pin, degrees):
-    servodegrees = scaler(0, 180, 530, 2400)
-    with open("/dev/servoblaster", "w") as f:
-        servovalue = int(servodegrees(degrees))
-        f.write("P1-{}={}us\n".format(physical_pin, servovalue))
+class CandyCrush:
+    def __init__(self):
+        self.servodegrees = scaler(0, 180, 530, 2400)
+        self.setup_servod()
 
-def main():
-    setup_servod()
-    set_servo(11, 0)
-    time.sleep(2)
-    set_servo(11, 180)
-    time.sleep(2)
-    set_servo(11, 90)
-    time.sleep(2)
-    set_servo(11, 45)
-    time.sleep(2)
-    set_servo(11, 30)
+    # Servo control
+    def setup_servod(self):
+        if not os.path.exists("/dev/servoblaster"):
+            subprocess.call(["servod", "--idle-timeout=1000ms"])
+
+    def set_servo(self, physical_pin, degrees):
+        with open("/dev/servoblaster", "w") as f:
+            servovalue = int(self.servodegrees(degrees))
+            f.write("P1-{}={}us\n".format(physical_pin, servovalue))
+
+    # Run!
+    def main(self):
+        self.set_servo(11, 0)
+        time.sleep(2)
+        self.set_servo(11, 180)
+        time.sleep(2)
+        self.set_servo(11, 90)
+        time.sleep(2)
+        self.set_servo(11, 45)
+        time.sleep(2)
+        self.set_servo(11, 30)
 
 if  __name__ =='__main__':
-    main()
+    cc = CandyCrush()
+    cc.main()
