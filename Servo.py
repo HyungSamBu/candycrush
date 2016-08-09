6edf74d847862c4f9ed2532129890ad4f2f41c72
Emil Vikström <emil@pixelstore.se>
2016-08-09 12:44:59 +0200

    Refactor servo control to Servo class

---
+++ Servo.py
@@ -0,0 +1,40 @@
+import os.path
+import subprocess
+import time
+
+class Servo:
+    def __init__(self, physical_pin, servo_speed_180, duty_min, duty_max):
+        self.setup_servod()
+        self.physical_pin = physical_pin
+        self.servo_speed_180 = servo_speed_180
+        self.servodegrees = scaler(0, 180, duty_min, duty_max)
+        self.position = 0                
+        
+    def setup_servod(self):
+        if not os.path.exists("/dev/servoblaster"):
+            subprocess.call(["servod", "--idle-timeout=2500ms"])
+
+    def set_servo(self, degrees):
+        with open("/dev/servoblaster", "w") as f:
+            servovalue = int(self.servodegrees(degrees))
+            f.write("P1-{}={}us\n".format(self.physical_pin, servovalue))
+
+        # Block during travel
+        travel_time = abs(self.position - degrees) / 180.0 * self.servo_speed_180
+        self.position = degrees
+        time.sleep(travel_time)
+
+    def set_servo_slowly(self, degrees, total_time):
+        travel_degrees = abs(self.position - degrees)
+        if travel_degrees == 0:
+            return
+        total_travel_time = travel_degrees / 180.0 * self.servo_speed_180
+        total_sleep_time = total_time - total_travel_time
+        if total_sleep_time <= 0:
+            self.set_servo(degrees)
+            return
+        tick_sleep_time = float(total_sleep_time) / travel_degrees
+        step = 1 if self.position < degrees else -1
+        for d in range(self.position, degrees + step, step):
+            self.set_servo(d)
+            time.sleep(tick_sleep_time)
--- candycrush.py
+++ candycrush.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 import ConfigParser
-import os.path
-import subprocess
 import time
+from Servo import Servo
 from tapioca_toggl import Toggl
 
 def scaler(OldMin, OldMax, NewMin, NewMax):
@@ -18,6 +17,7 @@ def configfile(config_path):
 class CandyCrush:
     def __init__(self, config):
         self.config = config
+        self.setup_servo(config)
         self.setup_apis(config)
         self.setup_servo_vars(config)
         self.setup_servod()
@@ -28,58 +28,26 @@ class CandyCrush:
         self.toggl = Toggl(access_token=toggl_token)
 
     # Servo control
-    def setup_servo_vars(self, config):
-        self.physical_pin = config.getint('Servo', 'physical_pin')
-        self.servo_speed_180 = config.getfloat('Servo', 'speed_180')
-        self.servodegrees = scaler(
-            0,
-            180,
-            config.getfloat('Servo', 'duty_min'),
-            config.getfloat('Servo', 'duty_max')
-        )
-        self.servo_last = 0        
-    
-    def setup_servod(self):
-        if not os.path.exists("/dev/servoblaster"):
-            subprocess.call(["servod", "--idle-timeout=2500ms"])
-
-    def set_servo(self, degrees):
-        with open("/dev/servoblaster", "w") as f:
-            servovalue = int(self.servodegrees(degrees))
-            f.write("P1-{}={}us\n".format(self.physical_pin, servovalue))
-
-        # Block during travel
-        travel_time = abs(self.servo_last - degrees) / 180.0 * self.servo_speed_180
-        self.servo_last = degrees
-        time.sleep(travel_time)
-
-    def set_servo_slowly(self, degrees, total_time):
-        travel_degrees = abs(self.servo_last - degrees)
-        if travel_degrees == 0:
-            return
-        total_travel_time = travel_degrees / 180.0 * self.servo_speed_180
-        total_sleep_time = total_time - total_travel_time
-        if total_sleep_time <= 0:
-            self.set_servo(degrees)
-            return
-        tick_sleep_time = float(total_sleep_time) / travel_degrees
-        step = 1 if self.servo_last < degrees else -1
-        for d in range(self.servo_last, degrees + step, step):
-            self.set_servo(d)
-            time.sleep(tick_sleep_time)
+    def setup_servo(self, config):
+        physical_pin = config.getint('Servo', 'physical_pin')
+        servo_speed_180 = config.getfloat('Servo', 'speed_180')
+        duty_min = config.getfloat('Servo', 'duty_min')
+        duty_max = config.getfloat('Servo', 'duty_max')
+        self.servo = Servo(physical_pin, servo_speed_180, duty_min, duty_max)
 
     def dispense_candy(self):
-        if not self.servo_last == 180:
-            self.set_servo(180)
+        servo = self.servo
+        if not servo.position == 180:
+            servo.set_servo(180)
             time.sleep(1)
-        self.set_servo_slowly(90, 1.5)
-        self.set_servo_slowly(0, 0.5)
+        servo.set_servo_slowly(90, 1.5)
+        servo.set_servo_slowly(0, 0.5)
         time.sleep(1)
-        self.set_servo_slowly(180, 1.5)
+        servo.set_servo_slowly(180, 1.5)
             
     # Run!
     def main(self):
-        self.set_servo(180)
+        self.servo.set_servo(180)
         self.dispense_candy()
 
 if  __name__ =='__main__':
